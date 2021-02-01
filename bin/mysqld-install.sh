23bfe91bb59524951d1b2083c498fc9a4214f46e
tca <tcarland@gmail.com>
2021-02-01 12:38:14 -0800

    set bind-address as  default var

--- bin/mysqld-install.sh
+++ bin/mysqld-install.sh
@@ -2,7 +2,7 @@
 #
 #
 PNAME=${0##*\/}
-MYSQL_ANSIBLE_VERSION="v21.01"
+MYSQL_ANSIBLE_VERSION="v21.02"
 
 playbook="mysqld-install.yml"
 tags="server5,client5"
@@ -12,11 +12,24 @@ env=
 # -------------------
 
 usage="
-Usage: $PNAME <action> <env>
+Wrapper script to the ansible-playbook 'mysqld-install' for 
+a given environment. By default this install MySQL 5.7, but 
+an install for mysql 8.0 can be set by passing 'server,client8' 
+tags to the playbook via the '--tags' option.
+
+Synopsis:
+  $PNAME <action> <env>
+
+Options:
+  -g|--group       :  Set a host 'limit' group for the run.
+  -h|--help        :  Show usage info and exit.
+  -T|--tags <tags> :  Set tags to control install version. 
+  -v|--verbose     :  Set increased verbosity in ansible.
+  -V|--version     :  Show version info and exit.
+
   <action> : any action other than 'run' is a 'dryrun'
   <env>    : is the ansible inventory name (under ./inventory)
 "
-
 version="$PNAME (mysql-ansible) $MYSQL_ANSIBLE_VERSION"
 
 # -------------------
